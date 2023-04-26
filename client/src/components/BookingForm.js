1eddf4c74020ea02c0f66a242842bb973c097b1b
Jon Minton <jon.will.minton@gmail.com>
2023-04-26 14:37:21 +0100

    do some work on client

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -1,13 +1,77 @@
-const BookingForm = () => {
+import React, {useState} from 'react';
 
+const BookingForm = ( {onGuestBookinSubmit} ) => {
+
+    const [guestName, setGuestName] = useState("");
+    const [guestEmail, setGuestEmail] = useState("");
+    const [guestCheckedIn, setGuestCheckedIn] = useState(false)
+
+    const handleGuestNameChange = (e) => {
+        // console.log(e.target.value)
+        setGuestName(e.target.value)
+    }
+
+    const handleGuestEmailChange = (e) => {
+        setGuestEmail(e.target.value)
+    }
+
+    const handleGuestCheckedInChange = (e) => {
+        setGuestCheckedIn(!guestCheckedIn)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e)
+        const newGuestDetails = {
+            guestName: guestName,
+            guestEmail: guestEmail,
+            guestCheckedIn: guestCheckedIn
+        }
+        console.log(`The new guest details... name: ${newGuestDetails.newGuestName}; email: ${newGuestDetails.newGuestEmail} and ${newGuestDetails.newGuestCheckedIn ? "is" : "is not"} checked in`)
+        onGuestBookinSubmit(newGuestDetails)
+        setGuestName("")
+        setGuestEmail("")
+        setGuestCheckedIn(false)
+    }
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     const guestNameValue = guestName.trim()
+    //     const guestEmailValue = guestEmail.trim()
+    //     const guestCheckedInValue = guestCheckedIn
+
+    //     if (!guestNameValue || !guestEmailValue) return // Return and do nothing if no valid input
+    //     // Update comments in CommentBox
+    //     onGuestBookinSubmit({
+    //         guestName: guestNameValue,
+    //         guestEmail: guestEmailValue,
+    //         checkedIn: guestCheckedInValue
+    //     })
+    //     // Clear the inputs
+    //     setGuestName('')
+    //     setGuestEmail('')
+    //     setGuestCheckedIn(false)
+    // }
 
 
 
 
     return (
         <>
-
             <h3>Booking form</h3>
+            <form onSubmit = {handleSubmit}>
+                <input type = "guestName" placeholder = "Guest Name" value = {guestName} onChange = {handleGuestNameChange}></input>
+                <input type = "email" placeholder = "Guest Email" value = {guestEmail} onChange = {handleGuestEmailChange}></input>
+                <input type="checkbox" id="checkbox" name = "checkbox" checked= {guestCheckedIn} onChange = {handleGuestCheckedInChange} />
+                <label htmlFor="checkbox"> Checked in? </label>
+                <button>Submit</button>
+            </form>
+            {/* <form onSubmit = {handleSubmit} >
+                <input type = "name" placeholder = "Guest Name" value = {guestName} onChange = {handleGuestNameChange}/>
+                <input type = "email" placeholder = "Guest Email" value = {guestEmail} onChange = {handleEmailChange}/>
+                <input type="checkbox" id="checkedIn" name="checkedIn" value={guestCheckedIn} onChange = {handleGuestCheckedin}/>
+                <button>Submit</button>
+            </form> */}
+
         </>
     )
 }
--- client/src/containers/HotelBox.js
+++ client/src/containers/HotelBox.js
@@ -1,19 +1,62 @@
+import React, {useState, useEffect} from 'react'
+
 import BookingsGrid from "../components/BookingsGrid"
 import BookingForm from "../components/BookingForm"
 
+
+// These are seed data from the client side. 
+// Once the database is set up these will be removed
+const seedDataClient = [
+    {
+        id: 1,
+        guestName: "Daffy",
+        guestEmail: "Daffy@duck.net",
+        checkedIn : false
+    },
+    {
+        id: 2,
+        guestName: "Mickey",
+        guestEmail: "Micky@mouse.net",
+        checkedIn : true
+    },
+    {
+        id: 3,
+        guestName: "Piggy",
+        guestEmail: "Piggy@mrs.net",
+        checkedIn : false
+    }
+]
+
 const HotelBox = () => {
 
+    const [bookings, setBookings] = useState(null)
 
+    useEffect( () => {
+        // Initally load client-side seed data 
+        // Later change so these are taken from the database
 
+        setBookings(seedDataClient)
+    }, [] )
 
+    const handleNewBooking = (newGuest) => {
+        console.log("handleNewBooking triggered")
+        // const maxId = Math.max(...(bookings.map(o => o.id)), 0);
+        // console.log(`The maximum id in the existing array is ${maxId}`)
+        // console.log(newGuest)
+        // newGuest.id = maxId + 1
+        setBookings(
+            ...bookings,
+            newGuest
+        )
+    }
 
 
 return (
     <>
     <h2> This is a hotelbox </h2>
-    <BookingForm></BookingForm>
-    <BookingsGrid></BookingsGrid>
-    </>
+    {bookings && <BookingForm onGuestBookinSubmit={handleNewBooking}/> }
+    {bookings && <BookingsGrid bookings={bookings}/>}
+    </> 
 )
 
 }
